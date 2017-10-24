81de43ffa6fc7a3db4d0c8617e7c1ff266dc2a22
Unknown <t-wanl@microsoft.com>
2017-10-24 16:45:24 +0800

    utils - generate split scene

--- caffe/utils/generate_training_data.py
+++ caffe/utils/generate_training_data.py
@@ -1,7 +1,19 @@
-import os, sys, argparse, glob
+import os, sys, argparse, glob, platform
+from collections import OrderedDict
 
-root = '/Volumes/xavier/dataset/sintel2/'
-dest_root_folder = './split_scene'
+
+if platform.system() == 'Darwin': 
+	root = '/Volumes/Transcend/dataset/sintel2/'
+	dest_root_folder = './split_scene'
+	caffe_root = 'home/lwp/workspace/direct-intrinsics/modified_caffe/caffe'
+	pretrained_model = '/home/lwp/workspace/caffe_model/vgg16.caffemodel'
+	template_root = '/Users/albertxavier/Box Sync/Graduation Project/graduation-project/caffe/utils/example_folder/template/'
+elif platform.system() == 'Linux':
+	root = None
+	dest_root_folder = None
+	caffe_root = 'home/lwp/workspace/direct-intrinsics/modified_caffe/caffe'
+	pretrained_model = '/home/lwp/workspace/caffe_model/vgg16.caffemodel'
+	template_root = None
 
 all_scenes  = glob.glob(os.path.join(root, 'clean/*'))
 for i, s in enumerate(all_scenes): 
@@ -77,29 +89,74 @@ for i in range(len(training_scenes)):
 	split_scene_folder = os.path.join(dest_root_folder, '{}'.format(*test_scenes[i]))
 	if not os.path.exists(split_scene_folder): os.makedirs(split_scene_folder)
 
-	training_folder = 'training/'
 
+	""" set file """
+	solver_file = os.path.join(split_scene_folder, 'solver.prototxt')
+	prototxt_file = os.path.join(split_scene_folder, 'train_val.prototxt')
+	snapshot_folder = os.path.join(split_scene_folder, 'snapshot', '')
 
 	""" generate training lists """
+	training_folder = 'training/'
 	training_source = 'list/'
 	if not os.path.exists(os.path.join(split_scene_folder, training_folder, training_source)): 
 		os.makedirs(os.path.join(split_scene_folder, training_folder, training_source))
 	
-	with open(os.path.join(split_scene_folder, training_folder, training_source, 'training.clean.except.{}.txt'.format(*test_scenes[i])), 'w') as f:
+	training_clean_file = os.path.join(split_scene_folder, training_folder, training_source, 'training.clean.except.{}.txt'.format(*test_scenes[i]))
+	training_albedo_file = os.path.join(split_scene_folder, training_folder, training_source, 'training.albedo.except.{}.txt'.format(*test_scenes[i]))
+	training_shading_file = os.path.join(split_scene_folder, training_folder, training_source, 'training.shading.except.{}.txt'.format(*test_scenes[i]))
+	with open(training_clean_file, 'w') as f:
 		f.write(training_clean_str)
-	with open(os.path.join(split_scene_folder, training_folder, training_source, 'training.albedo.except.{}.txt'.format(*test_scenes[i])), 'w') as f:
+	with open(training_albedo_file, 'w') as f:
 		f.write(training_albedo_str)
-	with open(os.path.join(split_scene_folder, training_folder, training_source, 'training.shading.except.{}.txt'.format(*test_scenes[i])), 'w') as f:
+	with open(training_shading_file, 'w') as f:
 		f.write(training_shading_str)
 
-	"""  """
+	""" generate solver """
+	solver = OrderedDict([
+		('net' , '"{}"'.format(prototxt_file)),
+		('base_lr' , 0.002),
+		('display' , 1),
+		('average_loss' , 1),
+		('max_iter' , 412500),
+		('momentum' , 0.9),
+		('weight_decay' , 1e-06),
+		('snapshot' , 5000),
+		('snapshot_prefix' , '"{}"'.format(snapshot_folder)),
+		('lr_policy' , '"poly"'),
+		('power' , 0.5),
+		('iter_size' , 1),
+		('share_blobs' , 'true')
+	])
+	
+	with open(solver_file, 'w') as f:
+		for (k,v) in solver.items():
+			f.write(k + ': ' + str(v) + '\n')
 
+	gpu = i % 3 + 1
+	""" generate train.sh """
+	if not os.path.exists(os.path.join(dest_root_folder, 'script', 'training')):
+		os.makedirs(os.path.join(dest_root_folder, 'script', 'training'))
+	with open(os.path.join(dest_root_folder, 'script', 'training', 'train_{}.sh'.format(*test_scenes[i])), 'w') as f:
+		f.write('#!/usr/bin/env sh\n')
+		f.write(os.path.join(caffe_root, 'build/tools/caffe') + ' train \\ \n')
+		f.write('-solver {} \\ \n'.format(solver_file))
+		f.write('-weights ' + pretrained_model + '\\ \n')
+		f.write('-gpu ' + str(gpu))
 
+	""" generate training prototxt """
+	prototxt = None
+	with open(os.path.join(template_root, 'train_val.prototxt'), 'r') as f:
+		prototxt = f.read()
+		prototxt = prototxt.replace('@@@clean@@@', "'"+training_clean_file+"'")
+		prototxt = prototxt.replace('@@@albedo@@@', "'"+training_albedo_file+"'")
+		prototxt = prototxt.replace('@@@shading@@@', "'"+training_shading_file+"'")
 
-	test_folder = 'test/'
+	with open(prototxt_file, 'w') as f:
+		f.write(prototxt)
+
 	
 	""" generate test lists """
-
+	test_folder = 'test/'
 	test_source = 'list/'
 	if not os.path.exists(os.path.join(split_scene_folder, test_folder, test_source)): 
 		os.makedirs(os.path.join(split_scene_folder, test_folder, test_source))
